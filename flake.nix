4970f90f3ba0b53a9c76eb1ed40adfda4a77c6c6
Ilyas Turki <turki.ilyass@gmail.com>
2026-03-18 18:17:29 +0100

    chore: bump version to v0.6.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      version = "0.5.1";
+      version = "0.6.0";
     in
     {
       packages.${system} = {
